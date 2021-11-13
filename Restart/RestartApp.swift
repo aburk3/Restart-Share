7b9c123b2ab2576275e4263e82a35270410a22a6
Austin Burke <austinlayneburke@gmail.com>
2021-11-13 15:22:23 -0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Austin Burke on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
