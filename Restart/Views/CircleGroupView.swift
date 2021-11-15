79faa85327dbbe63083d1a0b0d7231c933369db9
Austin Burke <austinlayneburke@gmail.com>
2021-11-15 06:06:58 -0600

    Added circle group component

--- Restart/Views/CircleGroupView.swift
+++ Restart/Views/CircleGroupView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct CircleGroupView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .stroke(.white.opacity(0.2), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(.white.opacity(0.2), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
     }
 }
 
 struct CircleGroupView_Previews: PreviewProvider {
     static var previews: some View {
-        CircleGroupView()
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView()
+        }
     }
 }
