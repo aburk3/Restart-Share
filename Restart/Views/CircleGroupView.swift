0e6ed6ad24f81a760a920e2cb6c09fdab984a95a
Austin Burke <austinlayneburke@gmail.com>
2021-11-15 06:06:33 -0600

    Added illustration and button to home view

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Austin Burke on 11/15/21.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleGroupView()
+    }
+}
