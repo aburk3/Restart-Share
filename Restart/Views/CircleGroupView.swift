610164413990c504468b3890845c4a9615112478
Austin Burke <austinlayneburke@gmail.com>
2021-11-15 06:11:21 -0600

    Added dynamic styling properties to circle group

--- Restart/Views/CircleGroupView.swift
+++ Restart/Views/CircleGroupView.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct CircleGroupView: View {
+    // MARK: - PROPERTY
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    // MARK: - BODY
+
     var body: some View {
         ZStack {
             Circle()
-                .stroke(.white.opacity(0.2), lineWidth: 40)
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
                 .frame(width: 260, height: 260, alignment: .center)
             Circle()
-                .stroke(.white.opacity(0.2), lineWidth: 80)
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
                 .frame(width: 260, height: 260, alignment: .center)
         } //: ZSTACK
     }
@@ -25,7 +32,8 @@ struct CircleGroupView_Previews: PreviewProvider {
         ZStack {
             Color("ColorBlue")
                 .ignoresSafeArea(.all, edges: .all)
-            CircleGroupView()
+            
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
         }
     }
 }
